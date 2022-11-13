0ba2db0c12bd6b379252f916bfe827427f10fa71
ThomasG233 <91529042+ThomasG233@users.noreply.github.com>
2022-11-13 17:38:19 +0000

    Updated for new additions to GridGen struct

--- grid.h
+++ grid.h
@@ -1,16 +1,19 @@
 #include <stddef.h>
-
 #define GRID_SIZE 31
 
 typedef struct gridGenerations {
-    int previousRow[GRID_SIZE];
-    int newRow[GRID_SIZE];
+    char previousRow[GRID_SIZE];
+    char newRow[GRID_SIZE];
+    int binary[8];
+    int ruleset;
+    int numOfGens;
 } GridGenerations;
 
-int initialiseRows(GridGenerations *gridPtr);
-void printRow(int row[]);
-int generateAutomaton();
+int initialiseGrid(GridGenerations *gridPtr);
+void printRow(char row[]);
+int generateAutomaton(GridGenerations *gPtr, char fileName[]);
 int collectGenerationInfo(int *ruleset, int *genNumber);
 int convertRulesetToBinary(int number, int *binary);
+int convertRulesetToDecimal(int binary[], int *number);
 int sendToFile();
 void writeRow();
